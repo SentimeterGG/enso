5935d72585c3b4add05b5f702765562db21c7825
Dimsum <aryasatyadimasfebri123@gmail.com>
2026-09-20 09:04:23 +0700

    segrknlj

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -57,4 +57,3 @@ func _on_animator_animation_finished(anim_name: StringName) -> void:
 			sc.refresh_od()
 			sc.reset()
 	%TransOffset.modulate = Color(1.0, 1.0, 1.0, Global.settingsData.bg_visibilty*0.01)
-
